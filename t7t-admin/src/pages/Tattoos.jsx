77780ca60eb374daf8cfed7f04516ed24be4a4d8
R0su <2019-104574@rtu.edu.ph>
2024-05-19 20:30:51 +0800

    Update Tattoos.jsx

--- t7t-admin/src/pages/Tattoos.jsx
+++ t7t-admin/src/pages/Tattoos.jsx
@@ -30,17 +30,10 @@ export default function Tattoos({open, handleOpen}) {
         <div className='flex justify-end px-20 pt-5'>
           <button className='text-black bg-white hover:bg-white/50 hover:text-white py-2 px-3 rounded-md' onClick={handleOpen}>Upload Image</button>
         </div>
-<<<<<<< Updated upstream
         <DataTable value={userTattooGallery} key='id' paginator rows={5} rowsPerPageOptions={[5, 10]} tableStyle={{ minWidth: '50rem' }} className='mx-auto w-[90%] mt-6' sortField='id' sortOrder={1}>
           <Column field="category" header="Category" style={{ width: '30%' }} className='first-letter:uppercase font-bold' />
           <Column body={imageBodyTemplate} header="Images" style={{ width: '30%' }} />
           <Column body={actionBodyTemplate} header="Action" style={{ width: '30%' }} />
-=======
-        <DataTable value={tattooGallery} key='id' paginator rows={5} rowsPerPageOptions={[5, 10]} tableStyle={{ minWidth: '50rem' }} className='mx-auto w-[90%] mt-6' sortField='id' sortOrder={1}>
-          <Column field="category" header="Category" style={{ width: '25%' }} className='first-letter:uppercase font-bold' />
-          <Column body={imageBodyTemplate} header="Images" style={{ width: '25%' }} />
-          <Column body={actionBodyTemplate} header="Action" style={{ width: '25%' }} />
->>>>>>> Stashed changes
         </DataTable>
       </div>
     </>
